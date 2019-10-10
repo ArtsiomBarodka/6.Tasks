e5d91537e5ea0df421d1009bd520fe1f34ba7d43
Artsiom <34836796+ArtsiomBarodka@users.noreply.github.com>
2019-10-10 22:52:55 +0300

    Add files via upload

---
+++ task1/properties/Constants.java
@@ -0,0 +1,14 @@
+/*
+ * 6.tasks
+ * Task 1
+ * Artsiom Barodka
+ *
+ */
+package tasks.task1.properties;
+
+public class Constants {
+    public final static String BOOKS_PATH = "src/main/resources/Books.txt";
+    public final static String PERSONS_PATH = "src/main/resources/Persons.txt";
+    public final static String HOST = "smtp.mail.ru";
+    public final static int PAGE_SIZE = 5;
+}
---
+++ task1/properties/Role.java
@@ -0,0 +1,12 @@
+/*
+ * 6.tasks
+ * Task 1
+ * Artsiom Barodka
+ *
+ */
+package tasks.task1.properties;
+
+public enum Role {
+    USER,
+    ADMIN
+}
